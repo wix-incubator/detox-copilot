7107c769ad44e3767f16c51e46c6e2a00c2798e7
lironsh <lironsh@wix.com>
2024-11-19 11:39:49 +0200

    test: add `fs` mocking util.

---
+++ src/test-utils/cache.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+export let mockedCacheFile: { [key: string]: any } | undefined;
+
+export const mockCache = (data: { [key: string]: any } | undefined = undefined) => {
+    mockedCacheFile = data;
+
+    (fs.writeFileSync as jest.Mock).mockImplementation((filePath, data) => {
+        mockedCacheFile = JSON.parse(data);
+    });
+
+    (fs.readFileSync as jest.Mock).mockReturnValue(JSON.stringify(mockedCacheFile));
+
+    (fs.existsSync as jest.Mock).mockReturnValue(mockedCacheFile !== undefined);
+};
