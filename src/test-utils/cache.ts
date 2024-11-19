b32d87014d76cbd6ab51ecbfc963d92deb6fabbe
lironsh <lironsh@wix.com>
2024-11-19 10:20:36 +0200

    test: Add file for usable mock definitionד

--- src/test-utils/cache.ts
+++ src/test-utils/cache.ts
@@ -1,8 +1,4 @@
 import fs from "fs";
-import path from "path";
-
-export const CACHE_FILE_NAME = 'test_cache.json';
-const CACHE_FILE_PATH = path.resolve(process.cwd(), CACHE_FILE_NAME);
 
 export let mockedCacheFile: { [key: string]: any } | undefined;
 
@@ -10,12 +6,10 @@ export const mockCache = (data: { [key: string]: any } | undefined = undefined) 
     mockedCacheFile = data;
 
     (fs.writeFileSync as jest.Mock).mockImplementation((filePath, data) => {
-        if (filePath === CACHE_FILE_PATH) {
-            mockedCacheFile = JSON.parse(data);
-        }
+        mockedCacheFile = JSON.parse(data);
     });
 
-    (fs.readFileSync as jest.Mock).mockReturnValueOnce(JSON.stringify(mockedCacheFile));
+    (fs.readFileSync as jest.Mock).mockReturnValue(JSON.stringify(mockedCacheFile));
 
     (fs.existsSync as jest.Mock).mockReturnValue(mockedCacheFile !== undefined);
 };
