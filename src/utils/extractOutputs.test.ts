4261f64ecc2ec84ea40de0284b4b0e469cad7c29
Asaf Korem <asaf.korem@gmail.com>
2025-01-25 19:56:32 +0200

    test: add missing unit tests.

--- src/utils/extractOutputs.test.ts
+++ src/utils/extractOutputs.test.ts
@@ -13,8 +13,8 @@ describe("extractOutputs", () => {
          Tap on GREAT button
         </ACTION>`;
     const outputsMapper: OutputsMapping = {
-      thoughts: "THOUGHTS",
-      action: "ACTION",
+      thoughts: { tag: "THOUGHTS", isRequired: true },
+      action: { tag: "ACTION", isRequired: true },
     };
     const outputs = extractOutputs({ text: textToBeParsed, outputsMapper });
     expect(outputs).toEqual({
@@ -37,13 +37,56 @@ describe("extractOutputs", () => {
          Tap on WOW button
         </ACTION>`;
     const outputsMapper: OutputsMapping = {
-      thoughts: "THOUGHTS",
-      action: "ACTION",
+      thoughts: { tag: "THOUGHTS", isRequired: true },
+      action: { tag: "ACTION", isRequired: true },
     };
     const outputs = extractOutputs({ text: textToBeParsed, outputsMapper });
     expect(outputs).toEqual({
       thoughts: "I think this is great",
       action: "Tap on GREAT button",
+    });
+  });
+
+  it("should throw error if required output is missing", () => {
+    const textToBeParsed = `
+        These are my thoughts:
+        <THOUGHTS>
+        I think this is great
+        </THOUGHTS>
+        This is the action the copilot should perform:
+         <ACTION>
+         Tap on GREAT button
+        </ACTION>`;
+    const outputsMapper: OutputsMapping = {
+      thoughts: { tag: "THOUGHTS", isRequired: true },
+      action: { tag: "ACTION", isRequired: true },
+      action2: { tag: "ACTION2", isRequired: true },
+    };
+    expect(() =>
+      extractOutputs({ text: textToBeParsed, outputsMapper }),
+    ).toThrowError("Missing field for required tag <ACTION2>");
+  });
+
+  it("should not throw error if required output is missing but not required", () => {
+    const textToBeParsed = `
+        These are my thoughts:
+        <THOUGHTS>
+        I think this is great
+        </THOUGHTS>
+        This is the action the copilot should perform:
+         <ACTION>
+         Tap on GREAT button
+        </ACTION>`;
+    const outputsMapper: OutputsMapping = {
+      thoughts: { tag: "THOUGHTS", isRequired: true },
+      action: { tag: "ACTION", isRequired: true },
+      action2: { tag: "ACTION2", isRequired: false },
+    };
+    const outputs = extractOutputs({ text: textToBeParsed, outputsMapper });
+    expect(outputs).toEqual({
+      thoughts: "I think this is great",
+      action: "Tap on GREAT button",
+      action2: "N/A",
     });
   });
 });
