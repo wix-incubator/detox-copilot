627a49e612691a1f52547935e1a733a5c4ff3556
Tzviel Solodar <tzviels@wix.com>
2025-01-25 12:35:41 +0200

    test: adding examples usage with different drivers wip

---
+++ examples/puppeteer/tests/Pilot.test.ts
@@ -0,0 +1,38 @@
+import * as puppeteer from 'puppeteer';
+import {setupBrowser} from '../setup/browserSetup';
+import copilot from '@copilot';
+import {PromptHandler} from '../../utils/promptHandler';
+import {PuppeteerPageApi} from '@copilot/drivers/puppeteer/index';
+
+describe('Example Test Suite', () => {
+  let browser: puppeteer.Browser;
+  let page: puppeteer.Page;
+  jest.setTimeout(300000); 
+
+  beforeAll(async () => {
+    const promptHandler : PromptHandler = new PromptHandler();
+    const { browser: b, page: p } = await setupBrowser(false);
+    const safeContext = {
+        evaluate: p.evaluate.bind(p),
+        $: p.$.bind(p),
+        $$: p.$$.bind(p),
+        content: p.content.bind(p),
+        screenshot: p.screenshot.bind(p),
+        hover: p.hover.bind(p),
+        page : p,
+      };
+    copilot.init(promptHandler, PuppeteerPageApi(safeContext));
+    copilot.start();
+    browser = b;
+    page = p;
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    copilot.end();
+  });
+
+  it('perform test with pilot', async () => {
+        await copilot.pilot('open https://example.com/ and press on more information');
+  });
+});
