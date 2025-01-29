c0c22a9547eba836da0ed20587cba5e28297e201
Tzviel Solodar <tzviels@wix.com>
2025-01-29 18:30:44 +0200

    apply lint

--- src/utils/ImageScaler.ts
+++ src/utils/ImageScaler.ts
@@ -2,38 +2,40 @@ import gm from "gm";
 const MAX_PIXELS = 2000000;
 
 export async function downscaleImage(imagePath: string): Promise<string> {
-return new Promise<string>((resolve, reject) => {
-    gm(imagePath)
-    .size(function (err: any, size: { width: number; height: number }) {
-        if (err) {
+  return new Promise<string>((resolve, reject) => {
+    gm(imagePath).size(function (
+      err: any,
+      size: { width: number; height: number },
+    ) {
+      if (err) {
         console.error("Error getting image size:", err);
         reject(err);
-        } else {
+      } else {
         const originalWidth = size.width;
         const originalHeight = size.height;
         const originalTotalPixels = originalWidth * originalHeight;
 
         if (originalTotalPixels <= MAX_PIXELS) {
-            resolve(imagePath);
+          resolve(imagePath);
         } else {
-            const aspectRatio = originalWidth / originalHeight;
-            const newHeight = Math.sqrt(MAX_PIXELS / aspectRatio);
-            const newWidth = newHeight * aspectRatio;
-            const roundedWidth = Math.round(newWidth);
-            const roundedHeight = Math.round(newHeight);
+          const aspectRatio = originalWidth / originalHeight;
+          const newHeight = Math.sqrt(MAX_PIXELS / aspectRatio);
+          const newWidth = newHeight * aspectRatio;
+          const roundedWidth = Math.round(newWidth);
+          const roundedHeight = Math.round(newHeight);
 
-            gm(imagePath)
+          gm(imagePath)
             .resize(roundedWidth, roundedHeight)
-                .write(imagePath, (err: any) => {
-                if (err) {
+            .write(imagePath, (err: any) => {
+              if (err) {
                 console.error("Error downscaling image:", err);
                 reject(err);
-                } else {
+              } else {
                 resolve(imagePath);
-                }
+              }
             });
         }
-        }
+      }
     });
-});
+  });
 }
